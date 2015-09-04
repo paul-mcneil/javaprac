84a2bf2031b41e8fa7d6cf77b181259e7183df30
Paul <paul.mcneil@rutgers.edu>
2015-09-04 16:43:08 -0400

    assignment2

---
+++ src/assignment2/Judge.java
@@ -0,0 +1,33 @@
+package assignment2;
+
+public class Judge {
+
+	private int ID;
+	private double score;
+	
+	public void setID(int jid) {
+		ID = jid;
+	}
+	
+	public int getID() {
+		return ID; // blah
+	}
+	
+	public void setScore(double s) {
+		score = s;
+	}
+	
+	public double getScore() {
+		return score;
+	}
+	
+	public boolean isValidScore(double score){
+		if(score >= 0 && score <= 10){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
+}
