99b3de8125cd874c5c6de158506a37e3c684c955
Paul <paul.mcneil@rutgers.edu>
2015-09-11 15:28:28 -0400

    hr

--- src/hackerrank/ICPC_Team.java
+++ src/hackerrank/ICPC_Team.java
@@ -14,8 +14,8 @@ public class ICPC_Team {
 		//String input = in.nextLine();
 		
 		
-		int numPeople = in.nextInt();
-		int numTopics = in.nextInt();
+		int numPeople = in.nextInt(); // 4
+		int numTopics = in.nextInt(); // 5
 		String[] lines = new String[numPeople];
 		for(int i = 0; i < numPeople; i++){
 			lines[i] = in.next();
@@ -32,7 +32,8 @@ public class ICPC_Team {
 				A[i][j] = temp[j];
 			}
 		}
-		
+		// numPeople = 4
+		// numTopics = 5
 		for(int i = 0; i < numPeople; i++){
 			for(int j = 0; j < numTopics; j++){
 				System.out.print(A[i][j]);
@@ -40,11 +41,66 @@ public class ICPC_Team {
 			System.out.println("");
 		}
 		int count = 0;
-		for(int i = 0; i < numPeople; i++){
+		int indexTracker = 0;
+
+		for(int i = 0; i < numPeople-1; i++){
 			for(int j = 0; j < numTopics; j++){
-				int k = j+1;
-				if(A[i][j] == A[k][j])
+				
+				for(int k = indexTracker; k < numPeople;k++){
+					
 					count++;
+					System.out.print("*Comparing ");
+					System.out.print("[" + i + "][" + j + "]");
+					System.out.print(" and " );
+					System.out.print("[" + (i+1) + "][" + j + "]");
+					System.out.println("");
+					
+					if(A[i][j] == A[i+1][j]){
+						
+					}
+					
+					if(A[i][j] == A[i][k]){
+						
+					}
+						
+				}
+				indexTracker++;
+				
+			
+				
+			/*	
+				System.out.print("Comparing ");
+				System.out.print("[" + i + "][" + j + "] = ");
+				System.out.print(A[i][j] + " and " );
+				System.out.print("[" + (i+1) + "][" + j + "] = " + A[i+1][j]);
+				System.out.println("");
+				
+				if(A[i][j] == A[i+1][j]){
+					//something
+				}
+							
+				for(int k = i+1; k < numPeople;k++){
+					count++;
+					System.out.print("*Comparing ");
+					System.out.print("[" + i + "][" + j + "]");
+					System.out.print(" and " );
+					System.out.print("[" + (k) + "][" + j + "]");
+					System.out.println("");
+					
+					if(A[i][j] == A[k][j]){
+						
+					}
+					
+					if(A[i][j] == A[i][k]){
+						
+					}
+						
+				}
+			*/	
+				
+				
+				
+				//count++;
 			}
 			System.out.println("");
 		}
